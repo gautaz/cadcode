55e5e64218d1016a7cab3a751c11d985514cc212
gautaz <11046232+gautaz@users.noreply.github.com>
2025-02-09 13:58:27 +0100

    add flush axis model

---
+++ models/flush_axis.py
@@ -0,0 +1,20 @@
+import logging
+from build123d import *
+
+logging.basicConfig(level=logging.INFO)
+
+axis_diameter, axis_length = 5, 95
+stop_diameter, stop_length = 10, 2
+
+with BuildPart() as axis:
+    with BuildSketch():
+        Circle(axis_diameter/2)
+    extrude(amount=axis_length)
+    with BuildSketch():
+        Circle(stop_diameter/2)
+    extrude(amount=stop_length)
+
+logging.info("Exporting...")
+exporter3d = Mesher()
+exporter3d.add_shape(axis.part)
+exporter3d.write("flush_axis.3mf")
