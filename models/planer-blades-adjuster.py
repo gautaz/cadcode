0f3da5dba61c667f29d584bd978b2498ee8597b5
gautaz <11046232+gautaz@users.noreply.github.com>
2025-05-29 11:27:22 +0200

    no need for handles & blade wedge

--- models/planer-blades-adjuster.py
+++ models/planer-blades-adjuster.py
@@ -5,60 +5,64 @@ from math import (
 from build123d import *
 
 length = 200
-thickness = 30
+thickness = 20
 width = 140
 
 handle_length = 100
 handle_width = 30
 handle_spacing = width/3
 
-wedge_height = 15
-wedge_thickness = 4
-wedge_hole_width = 10
+axis_wedge_height = 15
+axis_wedge_thickness = 4.4
+axis_wedge_hole_height = axis_wedge_height - 4
+axis_wedge_hole_width = 10
+
+blade_wedge_height = 1
+blade_wedge_width = 18
 
 
 with BuildPart() as mainframe:
     with BuildSketch():
-        RectangleRounded(length, width, radius=handle_width/2)
+        RectangleRounded(length, width, radius=15)
     extrude(amount=thickness)
 
 
-with BuildPart(mainframe.faces().sort_by(Axis.Z).first) as handle:
-    Box(handle_length, handle_width, thickness)
-    with GridLocations(handle_length, 0, 2, 1):
-        Cylinder(radius=handle_width/2, height=thickness)
-
-
-with BuildPart() as wedge:
+with BuildPart() as axis_wedge:
     with BuildSketch(mainframe.faces().sort_by(Axis.Z).last):
-        Rectangle(wedge_thickness, width)
-    extrude(amount=wedge_height)
+        Rectangle(axis_wedge_thickness, width)
+    extrude(amount=axis_wedge_height)
 
 
-with BuildPart() as wedge_hole_body:
-    with BuildSketch(wedge.faces().sort_by(Axis.Z).last):
-        Rectangle(wedge_thickness, wedge_hole_width)
-    extrude(amount=-wedge_height+wedge_hole_width/2)
+with BuildPart() as axis_wedge_hole_body:
+    with BuildSketch(axis_wedge.faces().sort_by(Axis.Z).last):
+        Rectangle(axis_wedge_thickness, axis_wedge_hole_width)
+    extrude(amount=-axis_wedge_hole_height+axis_wedge_hole_width/2)
 
 
-with BuildPart(wedge_hole_body.faces().sort_by(Axis.Z).first) as wedge_hole_head:
-    Cylinder(radius=5, height=wedge_thickness, rotation=(0, 90, 0))
+with BuildPart(axis_wedge_hole_body.faces().sort_by(Axis.Z).first) as axis_wedge_hole_head:
+    Cylinder(radius=5, height=axis_wedge_thickness, rotation=(0, 90, 0))
 
 
-with BuildPart() as wedge_hole:
-    add(wedge_hole_body.part)
-    add(wedge_hole_head.part)
+with BuildPart() as axis_wedge_hole:
+    add(axis_wedge_hole_body.part)
+    add(axis_wedge_hole_head.part)
+
+
+with BuildPart() as blade_wedge:
+    with BuildSketch(mainframe.faces().sort_by(Axis.Z).last):
+        Rectangle(blade_wedge_width, width)
+    extrude(amount=blade_wedge_height)
 
 
 with BuildPart() as adjuster:
     add(mainframe.part)
     with Locations((length/4, 0)):
-        add(wedge.part)
+        add(axis_wedge.part)
         with GridLocations(0, 55, 1, 3):
-            add(wedge_hole.part, mode=Mode.SUBTRACT)
-    with GridLocations(0, handle_spacing, 1, 2):
-        add(handle.part, mode=Mode.SUBTRACT)
-    fillet(adjuster.edges().group_by(Axis.Z)[0], radius=floor((handle_spacing-handle_width)/2))
+            add(axis_wedge_hole.part, mode=Mode.SUBTRACT)
+    with Locations((length/4-(blade_wedge_width+axis_wedge_thickness)/2, 0)):
+        add(blade_wedge.part)
+    fillet(adjuster.edges().group_by(Axis.Z)[0], radius=10)
     fillet(adjuster.edges().group_by(Axis.Z)[-1], radius=1)
 
 
